9c48f583f38aea504664e754796fb54346fcbdf4
Yashvardhan4197 <20BCS4197@cuchd.in>
2025-01-08 18:37:55 +0530

    tried adding grapple functionality

--- Assets/Scripts/PlayerService/PlayerManager/PlayerController.cs
+++ Assets/Scripts/PlayerService/PlayerManager/PlayerController.cs
@@ -6,11 +6,20 @@ public class PlayerController
 {
     private PlayerView playerView;
     private bool isDead;
+    private float grappleDistance;
+    private float grappleSpeed;
+    private bool isGrappling;
+    private Animator playerAnimator;
     public bool IsDead {  get { return isDead; } }
+    public float GrappleDistance {  get { return grappleDistance; } }
+    public bool IsGrappling { get { return isGrappling; } }
     public PlayerController(PlayerView playerView)
     {
         this.playerView = playerView;
         this.playerView.SetController(this);
+        playerAnimator = playerView.GetPlayerAnimator();
+        grappleDistance = 10f;
+        grappleSpeed = 5f;
     }
 
     public void OnGameStart()
@@ -22,8 +31,8 @@ public class PlayerController
     public void SetPlayerDirection(Vector3 mouseWorldPos)
     {
         Vector3 direction=(mouseWorldPos-playerView.transform.position).normalized;
-        playerView.GetPlayerAnimator().SetFloat("x", direction.x);
-        playerView.GetPlayerAnimator().SetFloat ("y", direction.y);
+        playerAnimator.SetFloat("x", direction.x);
+        playerAnimator.SetFloat ("y", direction.y);
     }
 
     public void SetPlayerAliveStatus(bool isAlive)
@@ -37,7 +46,30 @@ public class PlayerController
         {
             isDead = true;
         }
-        playerView.GetPlayerAnimator().SetBool("isDead", isDead);
+        playerAnimator.SetBool("isDead", isDead);
     }
 
+
+
+    public void SetIsGrapple(bool isGrapple)
+    {
+        isGrappling = isGrapple;
+    }
+
+
+    public void PullObject(Transform target)
+    {
+        Rigidbody2D rb2D = target.GetComponent<Rigidbody2D>();
+        if(rb2D!=null)
+        {
+            Debug.Log("hit");
+            rb2D.velocity = (playerView.transform.position - target.position).normalized * grappleSpeed;
+        }
+    }
+    public void EndGrapple(Transform grappledObjectTransform)
+    {
+        //playerView.GetLineRenderer().enabled = false;
+        SetIsGrapple(false);
+        grappledObjectTransform = null;
+    }
 }
--- Assets/Scripts/PlayerService/PlayerManager/PlayerView.cs
+++ Assets/Scripts/PlayerService/PlayerManager/PlayerView.cs
@@ -6,7 +6,12 @@ public class PlayerView : MonoBehaviour
 {
 
     private PlayerController playerController;
+    private Transform grappledObjectTransform;
+    private Vector2 currentDirection;
     [SerializeField] Animator playerAnimator;
+    [SerializeField] Transform grappleGunTransform;
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] LayerMask ignoreLayer;
     public void SetController(PlayerController playerController)
     {
         this.playerController = playerController;
@@ -19,11 +24,52 @@ public class PlayerView : MonoBehaviour
             Vector3 mousePos= Input.mousePosition;
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0));
             playerController?.SetPlayerDirection(mouseWorldPos);
+            Grapple(mouseWorldPos);
+            if(playerController.IsGrappling==true&&grappledObjectTransform!=null)
+            {
+                UpdateLineRenderer();
+            }
+        }
+    }
+
+    private void UpdateLineRenderer()
+    {
+        lineRenderer.SetPosition(0,grappleGunTransform.position);
+        lineRenderer.SetPosition(1,grappledObjectTransform.position);
+
+        if((Vector2)(this.transform.position-lineRenderer.GetPosition(1)).normalized!=currentDirection)
+        {
+            playerController.EndGrapple(grappledObjectTransform);
+        }
+    }
+
+
+
+    private void Grapple(Vector3 mouseWorldPos)
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray2D ray = new Ray2D();
+            ray.origin = grappleGunTransform.position;
+            ray.direction=((Vector2)mouseWorldPos-ray.origin).normalized;
+            RaycastHit2D hit= Physics2D.Raycast(ray.origin,ray.direction,playerController.GrappleDistance,~ignoreLayer);
+            if(hit.collider!=null)
+            {
+                
+                grappledObjectTransform = hit.transform;
+                playerController.SetIsGrapple(true);
+                playerController.PullObject(grappledObjectTransform);
+
+                lineRenderer.enabled = true;
+                lineRenderer.SetPosition(0,ray.origin);
+                currentDirection = ray.direction;
+            }
+
 
         }
     }
 
     public Animator GetPlayerAnimator() => playerAnimator;
-
+    public LineRenderer GetLineRenderer() => lineRenderer;  
 
 }
